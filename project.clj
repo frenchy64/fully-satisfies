d931cfad1dc67178275a8c2775e662d6c2dfec69
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-09-09 16:04:59 -0400

    fix

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns com.ambrose.fully-satisfies})
+  :repl-options {:init-ns io.github.frenchy64.fully-satisfies})

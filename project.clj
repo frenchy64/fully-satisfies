9a0dfb5f3ccacf0921cdc5a60e3bb30702b93f09
Wanderson Ferreira <wand@hey.com>
2021-09-09 19:27:57 -0300

    benchmark code

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:bench
+             {:source-paths ["scripts"]
+              :dependencies [[com.clojure-goes-fast/clj-async-profiler "0.5.1"]
+                             [criterium/criterium "0.4.6"]]
+              :jvm-opts ["-Djdk.attach.allowAttachSelf"]}}
   :repl-options {:init-ns io.github.frenchy64.fully-satisfies})

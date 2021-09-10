082722679fb4665c952a23af199eacf84bf72629
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-09-10 16:33:31 -0400

    Release :patch

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "--no-sign"]
-                  ["deploy"]
+                  ["deploy" "release"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]

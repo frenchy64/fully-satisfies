efc25015bb1af827704a819030e378c8d8b48f17
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-09-10 16:07:00 -0400

    deploy snapshots

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.frenchy64/fully-satisfies "1.0.0-SNAPSHOT"
+(defproject io.github.frenchy64/fully-satisfies "0.0.1-SNAPSHOT"
   :description "A variant of clojure.core/satisfies? that checks all methods are implemented."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -8,4 +8,9 @@
               :dependencies [[com.clojure-goes-fast/clj-async-profiler "0.5.1"]
                              [criterium/criterium "0.4.6"]]
               :jvm-opts ["-Djdk.attach.allowAttachSelf"]}}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password  :env/clojars_token
+                                    :sign-releases false}]]
+
   :repl-options {:init-ns io.github.frenchy64.fully-satisfies})

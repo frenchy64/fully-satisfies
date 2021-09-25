7770e1939be732ee3fe0d4fcabc6304fe967af96
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2021-09-25 19:10:19 -0400

    add each test

--- test/io/github/frenchy64/fully_satisfies/each_test.clj
+++ test/io/github/frenchy64/fully_satisfies/each_test.clj
@@ -1,14 +1,24 @@
 (ns io.github.frenchy64.fully-satisfies.each-test
   (:require [clojure.test :refer :all]
             [com.gfredericks.test.chuck.clojure-test :refer [checking]]
+            [clojure.test.check.generators :as gen]
             [io.github.frenchy64.fully-satisfies.each :refer [each]]))
 
+(defn test-each [c]
+  (testing c
+    (let [at (atom [])
+          _ (each #(swap! at conj %) c)]
+      (is (= (vec c) @at)))))
+
 (deftest each-test
-  (doseq [c [[]
+  (doseq [c [nil
+             []
              [:a]
              [1 2 3]
+             [1 (reduced 2) 3]
              (range 100)]]
-    (testing c
-      (let [at (atom [])
-            _ (each #(swap! at conj %) c)]
-        (is (= c @at))))))
+    (test-each c))
+  (checking
+    "each"
+    [c (gen/vector gen/any-equatable)]
+    (test-each c)))

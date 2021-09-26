da9fd8d826206382fd53b008b95aa5ce97f419d5
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2021-09-26 01:22:55 -0400

    add future'

---
+++ src/io/github/frenchy64/fully_satisfies/future.clj
@@ -0,0 +1,94 @@
+;   Copyright (c) Rich Hickey. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns io.github.frenchy64.fully-satisfies.future
+  "futures that clear conveyed bindings after execution."
+  (:refer-clojure :exclude [future future-call]))
+
+(set! *warn-on-reflection* true)
+
+(defn ^:private future'-binding-conveyor-fn
+  [f]
+  (let [frame (clojure.lang.Var/cloneThreadBindingFrame)]
+    (fn
+      ([]
+       (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+         (clojure.lang.Var/resetThreadBindingFrame frame)
+         (try
+           (f)
+           (finally
+             (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+      ([x]
+       (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+         (clojure.lang.Var/resetThreadBindingFrame frame)
+         (try
+           (f x)
+           (finally
+             (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+      ([x y]
+       (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+         (clojure.lang.Var/resetThreadBindingFrame frame)
+         (try
+           (f x y)
+           (finally
+             (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+      ([x y z]
+       (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+         (clojure.lang.Var/resetThreadBindingFrame frame)
+         (try
+           (f x y z)
+           (finally
+             (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+      ([x y z & args] 
+       (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+         (clojure.lang.Var/resetThreadBindingFrame frame)
+         (try
+           (apply f x y z args)
+           (finally
+             (clojure.lang.Var/resetThreadBindingFrame previous-frame))))))))
+
+(defn ^:private deref-future'
+  ([^java.util.concurrent.Future fut]
+     (.get fut))
+  ([^java.util.concurrent.Future fut timeout-ms timeout-val]
+     (try (.get fut timeout-ms java.util.concurrent.TimeUnit/MILLISECONDS)
+          (catch java.util.concurrent.TimeoutException e
+            timeout-val))))
+
+(defn future-call'
+  "Takes a function of no args and yields a future object that will
+  invoke the function in another thread, and will cache the result and
+  return it on all subsequent calls to deref/@. If the computation has
+  not yet finished, calls to deref/@ will block, unless the variant
+  of deref with timeout is used. See also - realized?."
+  [f]
+  (let [f (future'-binding-conveyor-fn f)
+        fut (.submit clojure.lang.Agent/soloExecutor ^Callable f)]
+    (reify 
+     clojure.lang.IDeref 
+     (deref [_] (deref-future' fut))
+     clojure.lang.IBlockingDeref
+     (deref
+      [_ timeout-ms timeout-val]
+      (deref-future fut timeout-ms timeout-val))
+     clojure.lang.IPending
+     (isRealized [_] (.isDone fut))
+    java.util.concurrent.Future
+      (get [_] (.get fut))
+      (get [_ timeout unit] (.get fut timeout unit))
+      (isCancelled [_] (.isCancelled fut))
+      (isDone [_] (.isDone fut))
+      (cancel [_ interrupt?] (.cancel fut interrupt?)))))
+  
+(defmacro future'
+  "Takes a body of expressions and yields a future object that will
+  invoke the body in another thread, and will cache the result and
+  return it on all subsequent calls to deref/@. If the computation has
+  not yet finished, calls to deref/@ will block, unless the variant of
+  deref with timeout is used. See also - realized?."
+  [& body] `(future-call' (^{:once true} fn* [] ~@body))) 
---
+++ test/io/github/frenchy64/fully_satisfies/future_test.clj
@@ -0,0 +1,96 @@
+;   Copyright (c) Rich Hickey. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns io.github.frenchy64.fully-satisfies.future-test
+  (:refer-clojure :exclude [future])
+  (:require [clojure.test :refer :all]
+            [io.github.frenchy64.fully-satisfies.future :refer [future']])
+  (:import [java.util.concurrent Executors ThreadFactory]))
+
+(def ^:dynamic *test-value* 1)
+
+(deftest future'-fn-properly-retains-conveyed-bindings
+  (let [a (atom [])]
+    (binding [*test-value* 2]
+      @(future' (dotimes [_ 3]
+                 ;; we need some binding to trigger binding pop
+                 (binding [*print-dup* false]
+                   (swap! a conj *test-value*)))
+               ;; after CLJ-2619 no pop is needed
+               (swap! a conj *test-value* @(future' *test-value*) @(future' @(future' *test-value*))))
+      (is (= (repeat 6 2) @a)))))
+
+(def ^:dynamic *my-var* :root-binding)
+
+;; this test runs a future and then attempts to retrieve the bindings on
+;; the future's thread after the future has finished. it might take a few tries
+;; for the second part to work. if it never does, prints a warning instead of failing.
+(deftest future'-cleans-up-binding-conveyance
+  (let [try-flaky-test
+        (fn []
+          (let [global-solo-executor clojure.lang.Agent/soloExecutor
+                thread-count (atom 0)
+                current-thread-prm (promise)
+                local-executor (Executors/newCachedThreadPool
+                                 (reify ThreadFactory
+                                   (newThread [_ runnable]
+                                     (swap! thread-count inc)
+                                     (Thread. runnable))))]
+            (try
+              (set-agent-send-off-executor! local-executor)
+              (let [f-prm (promise)
+                    f (binding [*my-var* :thread-binding
+                                *1 :foo]
+                        (future'
+                          (deliver current-thread-prm (Thread/currentThread))
+                          @f-prm))
+                    _ (set-agent-send-off-executor! global-solo-executor)
+                    ;; there's a chance other futures could have seen local-executor, so wait a bit to
+                    ;; let them start running. if this happens, since f is blocked it will force another
+                    ;; local-executor thread to be created. this will increment thread-count and 
+                    ;; trigger a retry on this test.
+                    _ (Thread/sleep 200)
+                    _ (deliver f-prm :done)
+                    _ @f
+                    _ (Thread/sleep 200) ;; encourage local-executor to reuse thread
+                    after-thread-bindings-prm (promise)
+                    result (deref (.submit local-executor
+                                           ^Callable
+                                           (fn* []
+                                             (if (= @current-thread-prm (Thread/currentThread))
+                                               (get-thread-bindings)
+                                               :wrong-thread)))
+                                  5000 ::timeout)
+                    thread-count @thread-count]
+                (cond
+                  (= 1 thread-count) result
+                  ;; something interfered
+                  :else :bad-thread-count))
+              (finally
+                (set-agent-send-off-executor! global-solo-executor)
+                (.shutdown local-executor)))))
+        flaky-test-result (reduce (fn [results _]
+                                    (let [maybe-flaky-result (try-flaky-test)]
+                                      (case maybe-flaky-result
+                                        ::timeout (throw (ex-info "Timeout" {:results (conj results maybe-flaky-result)}))
+                                        ;; retry
+                                        (:wrong-thread :bad-thread-count) (conj results maybe-flaky-result)
+                                        (reduced maybe-flaky-result))))
+                                  []
+                                  (range 50))]
+    (cond
+      (map? flaky-test-result) (let [actual-thread-bindings-after-future flaky-test-result]
+                                 (is (= {} actual-thread-bindings-after-future)))
+
+      (every? #{:wrong-thread :bad-thread-count} flaky-test-result)
+      (println (str "WARNING: " `future-cleans-up-binding-conveyance 
+                    " test was very unlucky"
+                    " and could not verify thread bindings. "
+                    flaky-test-result))
+
+      :else (throw (ex-info "Unknown result" {:result flaky-test-result})))))

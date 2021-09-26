af20889c197300d676bbbcaf0b7655f852325755
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2021-09-26 00:42:50 -0400

    doc

--- src/io/github/frenchy64/fully_satisfies/each.clj
+++ src/io/github/frenchy64/fully_satisfies/each.clj
@@ -2,6 +2,6 @@
   "An alternative to `clojure.core/run!` that does not short-circuit on reduced.")
 
 (defn each
-  "Invoke f on each (and every) subsequent element of c via reduce. Returns nil."
+  "Reduce over c and invoke f on each (and every) element. Returns nil."
   ([f c]
    (reduce #(do (f %2) nil) nil c)))

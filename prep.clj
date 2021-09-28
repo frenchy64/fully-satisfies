255bf152cb8f90ee2c2ac8610f332981f5ad9404
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2021-09-27 22:28:56 -0400

    add prep.clj

---
+++ prep.clj
@@ -0,0 +1,10 @@
+(ns prep)
+
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+
+(defn compile-java [_]
+  (b/javac {:src-dirs ["java"]
+            :class-dir class-dir
+            :basis basis
+            :javac-opts ["-source" "8" "-target" "8"]}))

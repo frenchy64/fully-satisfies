2459311bc0cf0e98f0f152e05bfe63d3ca8ffd24
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-09-12 00:12:33 -0400

    wip

--- src/io/github/frenchy64/fully_satisfies/unrolling_macro.clj
+++ src/io/github/frenchy64/fully_satisfies/unrolling_macro.clj
@@ -2,22 +2,6 @@
   "Utilities for unrolling functions."
   (:require [clojure.walk :as walk]))
 
-(defn maybe-apply [f fixed-args rest-args]
-  (if rest-args
-    `(apply ~f ~@fixed-args ~rest-args)
-    (list* f fixed-args)))
-
-(defn maybe-list* [fixed-args rest-args]
-  (if (seq fixed-args)
-    `(list* ~@fixed-args ~rest-args)
-    rest-args))
-
-(defn maybe-concat [& colls]
-  (when-some [rests (not-empty (remove nil? colls))]
-    (if (next rests)
-      `(concat ~@colls)
-      (first colls))))
-
 (defn single-char-syms-from [c]
   {:pre [(<= (int \a) (int c) (int \z))]}
   (concat (map (comp symbol str char)
@@ -157,47 +141,6 @@
 
 ;; all from clojure.core, for reference
 (comment
-(defn every-pred
-  "Takes a set of predicates and returns a function f that returns true if all of its
-  composing predicates return a logical true value against all of its arguments, else it returns
-  false. Note that f is short-circuiting in that it will stop execution on the first
-  argument that triggers a logical false result against the original predicates."
-  {:added "1.3"}
-  ([p]
-     (fn ep1
-       ([] true)
-       ([x] (boolean (p x)))
-       ([x y] (boolean (and (p x) (p y))))
-       ([x y z] (boolean (and (p x) (p y) (p z))))
-       ([x y z & args] (boolean (and (ep1 x y z)
-                                     (every? p args))))))
-  ([p1 p2]
-     (fn ep2
-       ([] true)
-       ([x] (boolean (and (p1 x) (p2 x))))
-       ([x y] (boolean (and (p1 x) (p1 y) (p2 x) (p2 y))))
-       ([x y z] (boolean (and (p1 x) (p1 y) (p1 z) (p2 x) (p2 y) (p2 z))))
-       ([x y z & args] (boolean (and (ep2 x y z)
-                                     (every? #(and (p1 %) (p2 %)) args))))))
-  ([p1 p2 p3]
-     (fn ep3
-       ([] true)
-       ([x] (boolean (and (p1 x) (p2 x) (p3 x))))
-       ([x y] (boolean (and (p1 x) (p1 y) (p2 x) (p2 y) (p3 x) (p3 y))))
-       ([x y z] (boolean (and (p1 x) (p1 y) (p1 z) (p2 x) (p2 y) (p2 z) (p3 x) (p3 y) (p3 z))))
-       ([x y z & args] (boolean (and (ep3 x y z)
-                                     (every? #(and (p1 %) (p2 %) (p3 %)) args))))))
-  ([p1 p2 p3 & ps]
-     (let [ps (list* p1 p2 p3 ps)]
-       (fn epn
-         ([] true)
-         ([x] (every? #(% x) ps))
-         ([x y] (every? #(and (% x) (% y)) ps))
-         ([x y z] (every? #(and (% x) (% y) (% z)) ps))
-         ([x y z & args] (boolean (and (epn x y z)
-                                       (every? #(every? % args) ps))))))))
-
-
 (defn some-fn
   "Takes a set of predicates and returns a function f that returns the first logical true value
   returned by one of its composing predicates against any of its arguments, else it returns
--- test/io/github/frenchy64/fully_satisfies/unrolling_macro_test.clj
+++ test/io/github/frenchy64/fully_satisfies/unrolling_macro_test.clj
@@ -3,13 +3,31 @@
             [clojure.test :refer [is]]
             [io.github.frenchy64.fully-satisfies.unrolling-macro
              :refer [defunrolled unrolled-fn-tail gensym-pretty prettify-unrolled
-                     maybe-apply
-                     maybe-list*
-                     maybe-concat
                      single-char-syms-from
                      uniformly-flowing-argvs]]))
 
 
+(defn maybe-apply [f fixed-args rest-args]
+  (if rest-args
+    `(apply ~f ~@fixed-args ~rest-args)
+    (list* f fixed-args)))
+
+(defn maybe-list* [fixed-args rest-args]
+  (if (seq fixed-args)
+    `(list* ~@fixed-args ~rest-args)
+    rest-args))
+
+(defn maybe-concat [& colls]
+  (when-some [rests (not-empty (remove nil? colls))]
+    (if (next rests)
+      `(concat ~@colls)
+      (first colls))))
+
+(defn maybe-and [coll]
+  (case (count coll)
+    0 true
+    1 (first coll)
+    `(and ~@coll)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -236,12 +254,6 @@
                                                                (list outer-f acc))
                                                              (maybe-apply (peek fixed-fs) fixed-args rest-args)
                                                              (pop fixed-fs)))})))))})
-
-(comment
-  (unrolled-fn-tail unrolled-comp-spec)
-  (prettify-unrolled (unrolled-fn-tail unrolled-comp-spec))
-  )
-
 (deftest unrolled-comp-spec-test
   (is (= (prettify-unrolled (unrolled-fn-tail (assoc unrolled-comp-spec :this 'unrolled-comp)))
          '(([] cc/identity)
@@ -476,19 +488,6 @@
            ([f arg1 arg2 arg3 & more]
             (cc/fn [& args] (cc/apply f arg1 arg2 arg3 (cc/concat args more))))))))
 
-
-(comment
-  (prettify-unrolled (unrolled-fn-tail unrolled-partial-spec))
-  (int \x)
-  (take 100 (single-char-syms-from \x))
-  (take 100 (map char 
-                 (drop
-                   (- (int \x) (int \a))
-                   (cycle (range (int \a) (inc (int \z)))))))
-  (int \z)
-  (- (int \z) (int \a))
-  )
-
 (defunrolled unrolled-partial
   "Takes a function f and fewer than the normal arguments to f, and
   returns a fn that takes a variable number of additional args. When
@@ -501,3 +500,87 @@
   (is (= (-> #'unrolled-partial meta :arglists)
          (-> #'clojure.core/partial meta :arglists)
          '([f] [f arg1] [f arg1 arg2] [f arg1 arg2 arg3] [f arg1 arg2 arg3 & more]))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; clojure.core/every-pred
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+#_
+(defn every-pred
+  "Takes a set of predicates and returns a function f that returns true if all of its
+  composing predicates return a logical true value against all of its arguments, else it returns
+  false. Note that f is short-circuiting in that it will stop execution on the first
+  argument that triggers a logical false result against the original predicates."
+  {:added "1.3"}
+  ([p]
+     (fn ep1
+       ([] true)
+       ([x] (boolean (p x)))
+       ([x y] (boolean (and (p x) (p y))))
+       ([x y z] (boolean (and (p x) (p y) (p z))))
+       ([x y z & args] (boolean (and (ep1 x y z)
+                                     (every? p args))))))
+  ([p1 p2]
+     (fn ep2
+       ([] true)
+       ([x] (boolean (and (p1 x) (p2 x))))
+       ([x y] (boolean (and (p1 x) (p1 y) (p2 x) (p2 y))))
+       ([x y z] (boolean (and (p1 x) (p1 y) (p1 z) (p2 x) (p2 y) (p2 z))))
+       ([x y z & args] (boolean (and (ep2 x y z)
+                                     (every? #(and (p1 %) (p2 %)) args))))))
+  ([p1 p2 p3]
+     (fn ep3
+       ([] true)
+       ([x] (boolean (and (p1 x) (p2 x) (p3 x))))
+       ([x y] (boolean (and (p1 x) (p1 y) (p2 x) (p2 y) (p3 x) (p3 y))))
+       ([x y z] (boolean (and (p1 x) (p1 y) (p1 z) (p2 x) (p2 y) (p2 z) (p3 x) (p3 y) (p3 z))))
+       ([x y z & args] (boolean (and (ep3 x y z)
+                                     (every? #(and (p1 %) (p2 %) (p3 %)) args))))))
+  ([p1 p2 p3 & ps]
+     (let [ps (list* p1 p2 p3 ps)]
+       (fn epn
+         ([] true)
+         ([x] (every? #(% x) ps))
+         ([x y] (every? #(and (% x) (% y)) ps))
+         ([x y z] (every? #(and (% x) (% y) (% z)) ps))
+         ([x y z & args] (boolean (and (epn x y z)
+                                       (every? #(every? % args) ps))))))))
+
+
+(def unrolled-partial-spec
+  {:argvs (let [rest-arity 4]
+            (assert (<= 2 rest-arity))
+            (cons '[p]
+                  (uniformly-flowing-argvs
+                    {:arities (range 2 (inc rest-arity))
+                     :fixed-names (map #(symbol (str "p" %)) (next (range)))
+                     :rest-name 'ps})))
+   :unrolled-arity (fn [_ fixed-preds rest-pred]
+                     (let [this (gensym-pretty (symbol (str "ep" (if rest-pred "n" (count fixed-preds)))))
+                           body `(fn ~epn
+                                   ~@(unrolled-fn-tail
+                                       {:argvs (uniformly-flowing-argvs
+                                                 {:arities (range 5)
+                                                  :fixed-names (single-char-syms-from \x)
+                                                  :rest-name 'args})
+                                        :unrolled-arity (fn [_ fixed-args rest-arg]
+                                                          ;; FIXME
+                                                          (let [p (gensym-pretty 'p)]
+                                                            (if rest-arg
+                                                              (if rest-pred
+                                                                (if (empty? fixed-args)
+                                                                  `(every? #(every? % ~rest-arg) ~rest-pred)
+                                                                  `(boolean (and (~this ~@fixed-args)
+                                                                                 (every? #(every? % ~rest-arg) ~rest-pred))))
+                                                                (if (empty? fixed-args)
+                                                                  `(every? (and (p1 %) (p2 %) (p3 %)) args)
+                                                                  `(every? (fn [~p] (and (~p x) (~p y) (~p z))) ~rest-pred)))
+                                                              (if rest-pred
+                                                                (if (seq fixed-args)
+                                                                  `(boolean (and ~@(mapcat (fn [p] (map #(list p %) fixed-args)) fixed-preds)))
+                                                                  true)))))}))]
+                       (if rest-pred
+                         `(let [~rest-pred ~(maybe-list* fixed-preds rest-pred)] ~body))))})

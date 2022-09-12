562ddc634d40ad325483a82d1d91a1e94c2da0b1
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-09-12 10:45:26 -0400

    fixme

--- src/io/github/frenchy64/fully_satisfies/everyp.clj
+++ src/io/github/frenchy64/fully_satisfies/everyp.clj
@@ -40,6 +40,7 @@
        ([x y] (boolean (and (p1 x) (p1 y) (p2 x) (p2 y))))
        ([x y z] (boolean (and (p1 x) (p1 y) (p1 z) (p2 x) (p2 y) (p2 z))))
        ([x y z & args] (boolean (and (ep2 x y z)
+                                     ;; FIXME this should be (and (every? p1 args) (every? p2 args))
                                      (every? #(and (p1 %) (p2 %)) args))))))
   ([p1 p2 p3]
      (fn ep3
@@ -48,6 +49,7 @@
        ([x y] (boolean (and (p1 x) (p1 y) (p2 x) (p2 y) (p3 x) (p3 y))))
        ([x y z] (boolean (and (p1 x) (p1 y) (p1 z) (p2 x) (p2 y) (p2 z) (p3 x) (p3 y) (p3 z))))
        ([x y z & args] (boolean (and (ep3 x y z)
+                                     ;; FIXME this should be (and (every? p1 args) (every? p2 args) (every? p3 args))
                                      (every? #(and (p1 %) (p2 %) (p3 %)) args))))))
   ([p1 p2 p3 & ps]
      (let [ps (list* p1 p2 p3 ps)]

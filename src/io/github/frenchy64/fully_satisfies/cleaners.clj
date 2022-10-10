c48ce7862dacfeba1ae3bc54bb92035557dfd524
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-10-10 15:12:09 -0400

    todo

--- src/io/github/frenchy64/fully_satisfies/cleaners.clj
+++ src/io/github/frenchy64/fully_satisfies/cleaners.clj
@@ -6,6 +6,9 @@
     (.register (Cleaner/create) v f)
     v))
 
+;; TODO take a function that can short-circuit walking until OOM
+;; eg., something that polls the number of cleaners
+;; expected to run in a particular test.
 (defn try-forcing-cleaners! []
   (try (doall (range))
        (catch OutOfMemoryError _)))

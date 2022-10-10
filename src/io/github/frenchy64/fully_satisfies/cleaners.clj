c274ec5c14d53b2241dbf79450b1ff18f3b16cd1
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-10-10 18:52:37 -0400

    tests

--- src/io/github/frenchy64/fully_satisfies/cleaners.clj
+++ src/io/github/frenchy64/fully_satisfies/cleaners.clj
@@ -6,9 +6,37 @@
     (.register (Cleaner/create) v f)
     v))
 
-;; TODO take a function that can short-circuit walking until OOM
-;; eg., something that polls the number of cleaners
-;; expected to run in a particular test.
-(defn try-forcing-cleaners! []
-  (try (doall (range))
-       (catch OutOfMemoryError _)))
+(defn try-forcing-cleaners!
+  ([] (try-forcing-cleaners! (constantly false)))
+  ([f] (let [c (volatile! (range))]
+         (try (loop [c @c]
+                (when-not (f)
+                  (prn "not" (first c))
+                  (System/gc)
+                  (recur (nthnext c 100000))))
+              (catch OutOfMemoryError _
+                (vreset! c nil)
+                (System/gc)
+                (println "OOM")))
+         (first @c)
+         nil)))
+
+(defn head-hold-detecting-lazy-seq
+  "Each element must be distinct according to identical?."
+  ([] (head-hold-detecting-lazy-seq (fn [i] (Object.))))
+  ([i->v]
+   (let [live (atom #{})
+         rec (fn rec [i]
+               (lazy-seq
+                 (let [v (i->v i)
+                       rst (rec (inc i))]
+                   (swap! live conj i)
+                   (prn live)
+                   (register-cleaner! v #(swap! live disj i))
+                   (reify clojure.lang.ISeq
+                     (first [this] v)
+                     (next [this] (seq rst))
+                     (more [this] rst)
+                     (seq [this] (cons v rst))))))]
+     {:lseq (rec 0)
+      :live live})))
--- test/io/github/frenchy64/fully_satisfies/cleaners_test.clj
+++ test/io/github/frenchy64/fully_satisfies/cleaners_test.clj
@@ -2,25 +2,53 @@
   "Goal: use Java Cleaners to test for memory leaks"
   (:require [clojure.test :refer [is]]
             [io.github.frenchy64.fully-satisfies.uncaught-testing-contexts :refer [testing deftest]]
-            [io.github.frenchy64.fully-satisfies.cleaners :refer [register-cleaner! try-forcing-cleaners!]]))
+            [io.github.frenchy64.fully-satisfies.cleaners :refer [register-cleaner! try-forcing-cleaners!
+                                                                  head-hold-detecting-lazy-seq]]))
 
+#_
 (deftest try-forcing-cleaners!-test
   (let [cleaned? (atom [])
         _ (doto (volatile! (register-cleaner! (Object.) #(swap! cleaned? conj true)))
             (vreset! nil))]
     (try-forcing-cleaners!)))
 
+(defn is-live [expected live]
+  (try-forcing-cleaners! #(= expected @live))
+  (is (= expected @live)))
+
+
+(deftest reduce2-processes-sequentially-after-first-test
+  (let [{:keys [live lseq]} (head-hold-detecting-lazy-seq)
+        times (atom 0)]
+    (reduce (fn [_ _]
+              (let [t (swap! times inc)]
+                (is-live (if (= 1 t)
+                           #{0 1}
+                           #{t})
+                         live)))
+            (take 10 lseq))
+    (is-live #{} live)))
+
 (deftest reduce3-processes-sequentially-test
-  (let [a (atom 0)
-        c (repeatedly 10 #(doto (hash-map :something (rand))
-                            (register-cleaner! (fn [] (swap! a inc)))))]
-    (reduce (fn [i c]
-              (let [i (do (try-forcing-cleaners!)
-                          (is (= i @a))
-                          (inc i))]
-                (println "Printing cleaner as evidence" c)
-                i))
-            0
-            c)
-    (try-forcing-cleaners!)
-    (is (= 10 @a))))
+  (let [{:keys [live lseq]} (head-hold-detecting-lazy-seq)]
+    (reduce (fn [i _]
+              (is-live #{i} live)
+              (inc i))
+            0 (take 10 lseq))
+    (is-live #{} live)))
+
+(deftest head-holding-test
+  (let [{:keys [live lseq]} (head-hold-detecting-lazy-seq)
+        head-holder (volatile! (doall (take 10 lseq)))]
+    (is-live (into #{} (range 10))
+             live)
+    (vreset! head-holder nil)
+    (is-live #{} live)))
+
+(deftest map-does-not-chunk-lazy-seq-test
+  (let [{:keys [live lseq]} (head-hold-detecting-lazy-seq)
+        head-holder (volatile! (doto (map identity lseq)
+                                 seq))]
+    (is-live #{0} live)
+    (vreset! head-holder nil)
+    (is-live #{} live)))

724e87f91cad517921d2bb43f0c36e75689214ae
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-11-23 00:14:01 -0500

    Fix #8: compensate for RT/intCast changes in Clojure 1.11

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :url "https://github.com/frenchy64/fully-satisfies"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :java-source-paths ["java"]
   :profiles {:bench
              {:source-paths ["scripts"]

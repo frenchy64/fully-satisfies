9481a00c1f164b827b70dec594a9347f05e0e14c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-11 14:17:14 -0500

    safer

---
+++ src/io/github/frenchy64/fully_satisfies/safer.clj
@@ -0,0 +1,17 @@
+(ns io.github.frenchy64.fully-satisfies.safer
+  (:refer-clojure :exclude [every?]))
+
+;;TODO unit test
+(defn every?
+  "Returns true if (pred x) is logical true for every x in coll, else
+  false."
+  {:tag Boolean
+   :added "1.0"
+   :static true}
+  [pred coll]
+  ;; reuses result of `seq` - Ambrose
+  (if-let [coll (seq coll)]
+    (if (pred (first coll))
+      (recur pred (next coll))
+      false)
+    true))

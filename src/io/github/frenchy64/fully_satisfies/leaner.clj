2ac5b52791534be4e1aabbf6f7430b59b2a425d4
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-11 17:00:07 -0500

    some

--- src/io/github/frenchy64/fully_satisfies/leaner.clj
+++ src/io/github/frenchy64/fully_satisfies/leaner.clj
@@ -1,5 +1,5 @@
 (ns io.github.frenchy64.fully-satisfies.leaner
-  (:refer-clojure :exclude [drop-while]))
+  (:refer-clojure :exclude [drop-while some]))
 
 (defn drop-while
   "Returns a lazy sequence of the items in coll starting from the
@@ -26,3 +26,15 @@
                     (recur pred (next s))
                     s))]
        (lazy-seq (step pred (seq coll))))))
+
+(defn some
+  "Returns the first logical true value of (pred x) for any x in coll,
+  else nil.  One common idiom is to use a set as pred, for example
+  this will return :fred if :fred is in the sequence, otherwise nil:
+  (some #{:fred} coll)"
+  {:added "1.0"
+   :static true}
+  [pred coll]
+  (loop [s (seq coll)] ;; only call seq at top of loop - Ambrose
+    (when s
+      (or (pred (first s)) (recur (next s))))))

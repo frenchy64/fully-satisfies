a0235cf0bc5d10a631ce8f133c1f20c9959eaf44
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-11 23:41:52 -0500

    drop is fine

--- src/io/github/frenchy64/fully_satisfies/lazier.clj
+++ src/io/github/frenchy64/fully_satisfies/lazier.clj
@@ -1,44 +1,6 @@
 (ns io.github.frenchy64.fully-satisfies.lazier
-  (:refer-clojure :exclude [cycle drop sequence dorun bounded-count
+  (:refer-clojure :exclude [cycle sequence dorun bounded-count
                             iterator-seq dedupe]))
-
-(when-not (= "true" (System/getProperty "io.github.frenchy64.fully-satisfies.safer.drop.no-1.12-perf-warn"))
-  (when (try (Class/forName "clojure.lang.IDrop")
-             (catch Throwable _))
-    (println "WARNING: io.github.frenchy64.fully-satisfies.safer/drop is missing 1.12 performance features")))
-
-(defn drop
-  "Returns a laziness-preserving sequence of all but the first n items in coll.
-  Returns a stateful transducer when no collection is provided."
-  {:added "1.0"
-   :static true}
-  ([n]
-     (fn [rf]
-       (let [nv (volatile! n)]
-         (fn
-           ([] (rf))
-           ([result] (rf result))
-           ([result input]
-              (let [n @nv]
-                (vswap! nv dec)
-                (if (pos? n)
-                  result
-                  (rf result input))))))))
-  ([n coll]
-     (do #_if #_(instance? clojure.lang.IDrop coll)
-       #_(or
-        (if (pos? n)
-          (.drop ^clojure.lang.IDrop coll (if (int? n) n (Math/ceil n)))
-          (seq coll))
-        ())
-       (let [step (fn [n coll]
-                    (if (pos? n) ;; don't check seq if not pos? - Ambrose
-                      (when-let [s (next coll)] ;; call next instead of seq+rest
-                        (recur (dec n) s))
-                      coll))]
-         (lazy-seq (step n coll))))))
-
-;;TODO unit test
 
 ;;TODO unit test
 ;; fix comma in docstring - Ambrose

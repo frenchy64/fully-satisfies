929fc2535b9158a1df6e435a356cffac4167b592
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-12 20:28:37 -0500

    [skip ci]

--- src/io/github/frenchy64/fully_satisfies/head_releasing.clj
+++ src/io/github/frenchy64/fully_satisfies/head_releasing.clj
@@ -1,17 +1,19 @@
 (ns io.github.frenchy64.fully-satisfies.head-releasing
   "Variants of clojure.core functions (and internal helpers)
-  that release the head of seqs above all other concerns."
-  )
+  that release the head of seqs above all other concerns.")
 
-;;TODO test
 (defn naive-seq-reduce
   "Reduces a seq, ignoring any opportunities to switch to a more
-  specialized implementation."
+  specialized implementation.
+  
+  Additionally, head-releasing/naive-seq-reduce does not retain
+  a strong reference to the element of s being passed to f while
+  calling f."
   [s f val]
   (loop [s (seq s)
          val val]
     (if s
-      (let [r (rest s) ;; release head of seq so f can own sf
+      (let [r (rest s) ;; release head of seq so f can own first
             ret (f val (first s))]
         (if (reduced? ret)
           @ret

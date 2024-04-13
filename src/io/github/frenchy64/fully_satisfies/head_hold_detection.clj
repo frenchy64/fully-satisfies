3c1521f3d053737c38932aa5e78518ea405ff68c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-12 23:30:53 -0500

    rename

---
+++ src/io/github/frenchy64/fully_satisfies/head_hold_detection.clj
@@ -0,0 +1,140 @@
+(ns io.github.frenchy64.fully-satisfies.head-hold-detection
+  "A framework to detect memory leaks via holding the head of sequences.
+
+  The java.lang.ref.Cleaner class (JDK9+) provides hooks into garbage
+  collection. You can register a function that is called when a value
+  becomes phantom reachable, indicating is it a candidate for garbage
+  collection.
+  
+  The JVM is very likely to perform garbage collection
+  right before throwing an OutOfMemoryError. Part of garbage
+  collection is calculating whether references are reachable.
+  We use this insight to force garbage collection (and hence, cleaners)
+  to run, by inducing an OutOfMemoryError in try-forcing-cleaners!.
+  
+  Tying these ideas together are head-holding seqs and the is-strong
+  testing macro. head-hold-detecting-lazy-seq returns a lazy seq
+  and an atom of all elements of the seq currently with strong references.
+  This seq can now be passed to a sequence-processing function you would
+  like to test for memory leaks.
+
+  is-strong then takes a set of seq indexes expected to have strong references
+  and checks them against the atom tracking strong references.
+
+  Here's an example of asserting that a program adds or subtracts strong references to elements
+  of a lazy seq at particular points.
+  
+  (deftest example-cleaners-test
+    (let [{:keys [strong lseq]} (head-hold-detecting-lazy-seq
+                                  {:n 10}) ;; seq of fresh Object's, length 10
+          ;; lseq=(...)
+          _ (is-strong #{} strong) ;; no elements currently in memory
+          lseq (seq lseq)
+          ;; lseq=(0 ...)
+          _ (is-strong #{0} strong) ;; just the first element in memory
+          _ (nnext lseq)
+          ;; lseq=(0 1 2...)
+          _ (is-strong #{0 1 2} strong) ;; the first 3 elements in memory
+          lseq (next lseq)
+          ;; lseq=(1 2 ...)
+          _ (is-strong #{1 2} strong) ;; the second 2 elements in memory
+          lseq (rest lseq)
+          ;; lseq=(2 ...)
+          _ (is-strong #{2} strong) ;; the third element in memory
+          lseq (rest lseq)
+          ;; lseq=(...)
+          _ (is-strong #{} strong) ;; no elements in memory
+          ;; lseq=(3 ...)
+          lseq (seq lseq)
+          _ (is-strong #{3} strong) ;; fourth element in memory
+          _ (class (first lseq)) ;; add a strong reference to lseq so previous line succeeds
+          ;; lseq=nil
+          _ (is-strong #{} strong) ;; lseq is entirely garbage collected
+          ]))
+
+  "
+  (:require [clojure.test :refer [is]]))
+
+(defmacro ^:private when-jdk9 [& body]
+  (when (try (Class/forName "java.lang.ref.Cleaner")
+             (catch Throwable _))
+    `(do ~@body)))
+
+(when-jdk9 (import [java.lang.ref Cleaner]))
+
+(when-jdk9
+  (defonce ^:private -cleaner (Cleaner/create))
+  (defn register-cleaner!
+    "Register a thunk to be called when object v
+    becomes phantom reachable."
+    [v f]
+    (let [^Runnable f #(f)]
+      (.register -cleaner v f)
+      v)))
+
+(when-jdk9
+  (defn try-forcing-cleaners!
+    "Induce an OutOfMemoryError in an attempt to force Cleaners,
+    including those registered by register-cleaner!."
+    ([] (try-forcing-cleaners! (constantly false)))
+    ([f] (try (loop [c []]
+                (System/gc)
+                (when-not (f)
+                  (recur (conj c (make-array Double/TYPE (dec Integer/MAX_VALUE))))))
+              (catch OutOfMemoryError _
+                #_(println "OOM")))
+     (f))))
+
+(when-jdk9
+  (defn head-hold-detecting-lazy-seq
+    "Returns a map with entries:
+    - :lseq, an lazy sequence of length n (default ##Inf) where each element is a distinct fresh Object entity, or 
+      the result of (i->v {:i <index>}) when provided. Returning :end key from i->v arg also ends the sequence.
+    - :strong, an atom containing a set of indicies whose values are (likely) currently strong references.
+    
+    For the most precise results, each element returned by i->v should be distinct according to identical?
+    from all other values in the current JVM environment."
+    ([] (head-hold-detecting-lazy-seq nil))
+    ([{:keys [i->v n]
+       :or {i->v (fn [{:keys [i end]}] (Object.))
+            n ##Inf}}]
+     (let [strong (atom #{})
+           rec (fn rec [i]
+                 (lazy-seq
+                   (when (< i n)
+                     (let [end (Object.)
+                           v (i->v {:i i :end end})]
+                       (when-not (identical? v end)
+                         (swap! strong conj i)
+                         (register-cleaner! v #(swap! strong disj i))
+                         (cons v (rec (inc i))))))))]
+       {:lseq (rec 0)
+        :strong strong}))))
+
+(when-jdk9
+  (defn head-hold-detecting-chunked-seq
+    "Each element must be distinct according to identical?."
+    ([] (head-hold-detecting-chunked-seq nil))
+    ([{:keys [i->v chunk-size]
+       :or {i->v (fn [i] (Object.))
+            chunk-size 32}}]
+     (let [strong (atom #{})
+           rec (fn rec [i]
+                 (lazy-seq
+                   (let [b (chunk-buffer chunk-size)]
+                     (dotimes [i chunk-size]
+                       (let [v (i->v i)]
+                         (swap! strong conj i)
+                         (register-cleaner! v #(swap! strong disj i))
+                         (chunk-append b v)))
+                     (chunk-cons (chunk b) (rec (inc i))))))]
+       {:lseq (rec 0)
+        :strong strong}))))
+
+(when-jdk9
+  (defn is-strong [expected strong]
+    (loop [tries 100]
+      (when (and (pos? tries)
+                 (not (try-forcing-cleaners! #(= expected @strong))))
+        (recur (dec tries))))
+    (is (= expected (into (sorted-set) @strong)))))

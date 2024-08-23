d38605c5157fe19173f4ea5523d52429cfc9ac2e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-08-22 19:26:39 -0500

    [skip ci]

--- test/io/github/frenchy64/fully_satisfies/configurable_core_macros_test_generated_cljfmt.clj
+++ test/io/github/frenchy64/fully_satisfies/configurable_core_macros_test_generated_cljfmt.clj
@@ -20,7 +20,10 @@
     fn]
    [io.github.frenchy64.fully-satisfies.configurable-core-macros.defn
     :as
-    defn]))
+    defn]
+   [io.github.frenchy64.fully-satisfies.configurable-core-macros.defmacro
+    :as
+    defmacro]))
 
 (clojure.core/defmacro my-let
   "binding => binding-form init-expr\n     binding-form => name, or destructuring-form\n     destructuring-form => map-destructure-form, or seq-destructure-form\n\n     Evaluates the exprs in a lexical context in which the symbols in\n     the binding-forms are bound to their respective init-exprs or parts\n     therein.\n\n     See https://clojure.org/reference/special_forms#binding-forms for\n     more information about destructuring."
@@ -32,7 +35,8 @@
    '{:rename
      {clojure.core/let my-let,
       clojure.core/fn my-fn,
-      clojure.core/defn my-defn},
+      clojure.core/defn my-defn,
+      clojure.core/defmacro my-defmacro},
      :opts-var
      io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))
 
@@ -47,26 +51,46 @@
    '{:rename
      {clojure.core/let my-let,
       clojure.core/fn my-fn,
-      clojure.core/defn my-defn},
+      clojure.core/defn my-defn,
+      clojure.core/defmacro my-defmacro},
      :opts-var
      io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))
 
 (def ^{:doc "Same as (def name (fn [params* ] exprs*)) or (def\n                             name (fn ([params* ] exprs*)+)) with any doc-string or attrs added\n                             to the var metadata. prepost-map defines a map with optional keys\n                             :pre and :post that contain collections of pre or post conditions.", :arglists (quote ([name doc-string? attr-map? [params*] prepost-map? body] [name doc-string? attr-map? ([params*] prepost-map? body) + attr-map?]))} my-defn
-  (clojure.core/fn my-defn [&form__2192__auto__
-                            &env__2193__auto__
-                            name__2194__auto__
+  (clojure.core/fn my-defn [&form__17141__auto__
+                            &env__17142__auto__
+                            name__17143__auto__
                             &
-                            fdecl__2195__auto__]
+                            fdecl__17144__auto__]
     (io.github.frenchy64.fully-satisfies.configurable-core-macros.defn/defn-implementation
-      &form__2192__auto__
-      &env__2193__auto__
-      name__2194__auto__
-      fdecl__2195__auto__
+      name__17143__auto__
+      fdecl__17144__auto__
+      false
       '{:rename
         {clojure.core/let my-let,
          clojure.core/fn my-fn,
-         clojure.core/defn my-defn},
+         clojure.core/defn my-defn,
+         clojure.core/defmacro my-defmacro},
         :opts-var
         io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts})))
 
 (clojure.core/doto #'my-defn .setMacro)
+
+(def ^{:doc "Like defn, but the resulting function name is declared as a\n                           macro and will be used as a macro by the compiler when it is\n                           called.", :arglists (quote ([name doc-string? attr-map? [params*] body] [name doc-string? attr-map? ([params*] body) + attr-map?]))} my-defmacro
+  (clojure.core/fn [&form__17173__auto__
+                    &env__17174__auto__
+                    name__17175__auto__
+                    &
+                    args__17176__auto__]
+    (io.github.frenchy64.fully-satisfies.configurable-core-macros.defmacro/defmacro-implementation
+      name__17175__auto__
+      args__17176__auto__
+      '{:rename
+        {clojure.core/let my-let,
+         clojure.core/fn my-fn,
+         clojure.core/defn my-defn,
+         clojure.core/defmacro my-defmacro},
+        :opts-var
+        io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts})))
+
+(clojure.core/doto #'my-defmacro .setMacro)
--- test/io/github/frenchy64/fully_satisfies/configurable_core_macros_test_generated_zprint.clj
+++ test/io/github/frenchy64/fully_satisfies/configurable_core_macros_test_generated_zprint.clj
@@ -14,8 +14,9 @@
     [clojure.core :as cc]
     [io.github.frenchy64.fully-satisfies.configurable-core-macros.let :as let]
     [io.github.frenchy64.fully-satisfies.configurable-core-macros.fn :as fn]
-    [io.github.frenchy64.fully-satisfies.configurable-core-macros.defn :as
-     defn]))
+    [io.github.frenchy64.fully-satisfies.configurable-core-macros.defn :as defn]
+    [io.github.frenchy64.fully-satisfies.configurable-core-macros.defmacro :as
+     defmacro]))
 
 (clojure.core/defmacro my-let
   "binding => binding-form init-expr\n     binding-form => name, or destructuring-form\n     destructuring-form => map-destructure-form, or seq-destructure-form\n\n     Evaluates the exprs in a lexical context in which the symbols in\n     the binding-forms are bound to their respective init-exprs or parts\n     therein.\n\n     See https://clojure.org/reference/special_forms#binding-forms for\n     more information about destructuring."
@@ -27,7 +28,8 @@
     (quote
       {:rename {clojure.core/let my-let,
                 clojure.core/fn my-fn,
-                clojure.core/defn my-defn},
+                clojure.core/defn my-defn,
+                clojure.core/defmacro my-defmacro},
        :opts-var
          io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts})))
 
@@ -41,7 +43,8 @@
     (quote
       {:rename {clojure.core/let my-let,
                 clojure.core/fn my-fn,
-                clojure.core/defn my-defn},
+                clojure.core/defn my-defn,
+                clojure.core/defmacro my-defmacro},
        :opts-var
          io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts})))
 
@@ -52,18 +55,40 @@
                       [name doc-string? attr-map? ([params*] prepost-map? body)
                        + attr-map?]))}
   my-defn
-  (clojure.core/fn my-defn [&form__2192__auto__ &env__2193__auto__
-                            name__2194__auto__ & fdecl__2195__auto__]
+  (clojure.core/fn my-defn [&form__17141__auto__ &env__17142__auto__
+                            name__17143__auto__ & fdecl__17144__auto__]
     (io.github.frenchy64.fully-satisfies.configurable-core-macros.defn/defn-implementation
-      &form__2192__auto__
-      &env__2193__auto__
-      name__2194__auto__
-      fdecl__2195__auto__
+      name__17143__auto__
+      fdecl__17144__auto__
+      false
       (quote
         {:rename {clojure.core/let my-let,
                   clojure.core/fn my-fn,
-                  clojure.core/defn my-defn},
+                  clojure.core/defn my-defn,
+                  clojure.core/defmacro my-defmacro},
          :opts-var
            io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))))
 
 (clojure.core/doto (var my-defn) .setMacro)
+
+(def
+  ^{:doc
+      "Like defn, but the resulting function name is declared as a\n                           macro and will be used as a macro by the compiler when it is\n                           called.",
+    :arglists (quote ([name doc-string? attr-map? [params*] body]
+                      [name doc-string? attr-map? ([params*] body) +
+                       attr-map?]))}
+  my-defmacro
+  (clojure.core/fn [&form__17173__auto__ &env__17174__auto__ name__17175__auto__
+                    & args__17176__auto__]
+    (io.github.frenchy64.fully-satisfies.configurable-core-macros.defmacro/defmacro-implementation
+      name__17175__auto__
+      args__17176__auto__
+      (quote
+        {:rename {clojure.core/let my-let,
+                  clojure.core/fn my-fn,
+                  clojure.core/defn my-defn,
+                  clojure.core/defmacro my-defmacro},
+         :opts-var
+           io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))))
+
+(clojure.core/doto (var my-defmacro) .setMacro)

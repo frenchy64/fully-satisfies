11e5e75ce7ad91c4790754084b1049761ea8e0bd
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-08-23 19:01:51 -0500

    gen

--- test/io/github/frenchy64/fully_satisfies/configurable_core_macros_test_generated_pprint.clj
+++ test/io/github/frenchy64/fully_satisfies/configurable_core_macros_test_generated_pprint.clj
@@ -34,10 +34,11 @@
 (clojure.core/defmacro my-let
   "binding => binding-form init-expr\n     binding-form => name, or destructuring-form\n     destructuring-form => map-destructure-form, or seq-destructure-form\n\n     Evaluates the exprs in a lexical context in which the symbols in\n     the binding-forms are bound to their respective init-exprs or parts\n     therein.\n\n     See https://clojure.org/reference/special_forms#binding-forms for\n     more information about destructuring."
   {:forms '[(let [bindings*] exprs*)]}
-  [bindings__2129__auto__ & body__2130__auto__]
+  [bindings__2656__auto__ & body__2657__auto__]
   (io.github.frenchy64.fully-satisfies.configurable-core-macros.let/let-implementation
-    bindings__2129__auto__
-    body__2130__auto__
+    &form
+    bindings__2656__auto__
+    body__2657__auto__
     '{:rename
       {clojure.core/let my-let,
        clojure.core/fn my-fn,
@@ -52,10 +53,10 @@
   "params => positional-params*, or positional-params* & rest-param\n     positional-param => binding-form\n     rest-param => binding-form\n     binding-form => name, or destructuring-form\n\n     Defines a function.\n\n     See https://clojure.org/reference/special_forms#fn for more information"
   {:forms
    '[(fn name? [params*] exprs*) (fn name? ([params*] exprs*) +)]}
-  [& sigs__2159__auto__]
+  [& sigs__15093__auto__]
   (io.github.frenchy64.fully-satisfies.configurable-core-macros.fn/fn-implementation
     &form
-    sigs__2159__auto__
+    sigs__15093__auto__
     '{:rename
       {clojure.core/let my-let,
        clojure.core/fn my-fn,
@@ -67,14 +68,14 @@
       io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))
 
 (def ^{:doc "Same as (def name (fn [params* ] exprs*)) or (def\n                             name (fn ([params* ] exprs*)+)) with any doc-string or attrs added\n                             to the var metadata. prepost-map defines a map with optional keys\n                             :pre and :post that contain collections of pre or post conditions.", :arglists (quote ([name doc-string? attr-map? [params*] prepost-map? body] [name doc-string? attr-map? ([params*] prepost-map? body) + attr-map?]))} my-defn
- (clojure.core/fn my-defn [&form__17546__auto__
-                           &env__17547__auto__
-                           name__17548__auto__
+ (clojure.core/fn my-defn [&form__15126__auto__
+                           &env__15127__auto__
+                           name__15128__auto__
                            &
-                           fdecl__17549__auto__]
+                           fdecl__15129__auto__]
    (io.github.frenchy64.fully-satisfies.configurable-core-macros.defn/defn-implementation
-     name__17548__auto__
-     fdecl__17549__auto__
+     name__15128__auto__
+     fdecl__15129__auto__
      false
      '{:rename
        {clojure.core/let my-let,
@@ -89,14 +90,14 @@
 (clojure.core/doto #'my-defn .setMacro)
 
 (def ^{:doc "Like defn, but the resulting function name is declared as a\n                           macro and will be used as a macro by the compiler when it is\n                           called.", :arglists (quote ([name doc-string? attr-map? [params*] body] [name doc-string? attr-map? ([params*] body) + attr-map?]))} my-defmacro
- (clojure.core/fn [&form__17173__auto__
-                   &env__17174__auto__
-                   name__17175__auto__
+ (clojure.core/fn [&form__15156__auto__
+                   &env__15157__auto__
+                   name__15158__auto__
                    &
-                   args__17176__auto__]
+                   args__15159__auto__]
    (io.github.frenchy64.fully-satisfies.configurable-core-macros.defmacro/defmacro-implementation
-     name__17175__auto__
-     args__17176__auto__
+     name__15158__auto__
+     args__15159__auto__
      '{:rename
        {clojure.core/let my-let,
         clojure.core/fn my-fn,
@@ -112,14 +113,14 @@
 (clojure.core/defmacro my-defmethod
   "Creates and installs a new method of multimethod associated with dispatch-value. "
   {:arglists '([multifn dispatch-val & fn-tail])}
-  [multifn__18116__auto__
-   dispatch-val__18117__auto__
+  [multifn__15176__auto__
+   dispatch-val__15177__auto__
    &
-   fn-tail__18118__auto__]
+   fn-tail__15178__auto__]
   (io.github.frenchy64.fully-satisfies.configurable-core-macros.defmethod/defmethod-implementation
-    multifn__18116__auto__
-    dispatch-val__18117__auto__
-    fn-tail__18118__auto__
+    multifn__15176__auto__
+    dispatch-val__15177__auto__
+    fn-tail__15178__auto__
     '{:rename
       {clojure.core/let my-let,
        clojure.core/fn my-fn,
@@ -131,13 +132,13 @@
       io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))
 
 (clojure.core/defmacro my-if-let
-  "bindings => binding-form test\n\n       If test is true, evaluates then with binding-form bound to the value of \n       test, if not, yields else"
+  "bindings => binding-form test\n\n     If test is true, evaluates then with binding-form bound to the value of \n     test, if not, yields else"
   {:added "1.0", :arglists '([bindings then] [bindings then else])}
-  ([bindings__18896__auto__ then__18897__auto__]
+  ([bindings__2227__auto__ then__2228__auto__]
     (io.github.frenchy64.fully-satisfies.configurable-core-macros.if-let/if-let-implementation
       &form
-      bindings__18896__auto__
-      then__18897__auto__
+      bindings__2227__auto__
+      then__2228__auto__
       '{:rename
         {clojure.core/let my-let,
          clojure.core/fn my-fn,
@@ -147,17 +148,17 @@
          clojure.core/if-let my-if-let},
         :opts-var
         io.github.frenchy64.fully-satisfies.configurable-core-macros-test/opts}))
-  ([bindings__18896__auto__
-    then__18897__auto__
-    else__18898__auto__
+  ([bindings__2227__auto__
+    then__2228__auto__
+    else__2229__auto__
     &
-    oldform__18899__auto__]
+    oldform__2230__auto__]
     (io.github.frenchy64.fully-satisfies.configurable-core-macros.if-let/if-let-implementation
       &form
-      bindings__18896__auto__
-      then__18897__auto__
-      else__18898__auto__
-      oldform__18899__auto__
+      bindings__2227__auto__
+      then__2228__auto__
+      else__2229__auto__
+      oldform__2230__auto__
       '{:rename
         {clojure.core/let my-let,
          clojure.core/fn my-fn,

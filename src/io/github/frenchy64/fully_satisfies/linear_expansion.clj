265aa2466a529dea96cecbdcaf17368c8315ebf0
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-27 14:34:45 -0600

    doseq/for with linear expansion size (instead of exponential)

---
+++ src/io/github/frenchy64/fully_satisfies/linear_expansion.clj
@@ -0,0 +1,153 @@
+;; Copyright (c) Rich Hickey. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns io.github.frenchy64.fully-satisfies.linear-expansion
+  "Macros whose expansions grow linearly rather than exponentially."
+  (:refer-clojure :exclude [doseq for])
+  (:require [io.github.frenchy64.fully-satisfies.configurable-core-macros.assert-args :refer [assert-args]]))
+
+(defmacro doseq
+  "Repeatedly executes body (presumably for side-effects) with
+  bindings and filtering as provided by \"for\".  Does not retain
+  the head of the sequence. Returns nil.
+  
+  Unlike clojure.core/doseq, expansion grows linearly with the number
+  of nestings of this macro, rather than exponentially."
+  [seq-exprs & body]
+  (assert-args
+    (vector? seq-exprs) "a vector for its binding"
+    (even? (count seq-exprs)) "an even number of forms in binding vector")
+  (let [step (fn step [recform exprs]
+               (if-not exprs
+                 [true `(do ~@body)]
+                 (let [k (first exprs)
+                       v (second exprs)]
+                   (if (keyword? k)
+                     (let [steppair (step recform (nnext exprs))
+                           needrec (steppair 0)
+                           subform (steppair 1)]
+                       (cond
+                         (= k :let) [needrec `(let ~v ~subform)]
+                         (= k :while) [false `(when ~v
+                                                ~subform
+                                                ~@(when needrec [recform]))]
+                         (= k :when) [false `(if ~v
+                                               (do
+                                                 ~subform
+                                                 ~@(when needrec [recform]))
+                                               ~recform)]))
+                     (let [seq- (gensym "seq_")
+                           chunk- (with-meta (gensym "chunk_")
+                                             {:tag 'clojure.lang.IChunk})
+                           count- (gensym "count_")
+                           i- (gensym "i_")
+                           in-chunk- (gensym "in-chunk_")
+                           recform `(if ~in-chunk-
+                                      (recur ~seq- ~chunk- ~count- (unchecked-inc ~i-))
+                                      (recur (next ~seq-) nil 0 0))
+                           steppair (step recform (nnext exprs))
+                           needrec (steppair 0)
+                           subform (steppair 1)]
+                       [true
+                        `(loop [~seq- (seq ~v), ~chunk- nil,
+                                ~count- 0, ~i- 0]
+                           (let [~in-chunk- (< ~i- ~count-)
+                                 ~seq- (if ~in-chunk- ~seq- (seq ~seq-))]
+                             (when (if ~in-chunk- true ~seq-)
+                               (let [chunked?# (if ~in-chunk- false (chunked-seq? ~seq-))
+                                     ~k (if ~in-chunk-
+                                          (.nth ~chunk- ~i-)
+                                          (if chunked?# nil (first ~seq-)))]
+                                 (if (if ~in-chunk- false chunked?#)
+                                   (let [c# (chunk-first ~seq-)]
+                                     (recur (chunk-rest ~seq-) c#
+                                            (int (count c#)) (int 0)))
+                                   (do ~subform
+                                       ~@(when needrec [recform])))))))])))))]
+    (nth (step nil (seq seq-exprs)) 1)))
+
+(defmacro for
+  "List comprehension. Takes a vector of one or more
+   binding-form/collection-expr pairs, each followed by zero or more
+   modifiers, and yields a lazy sequence of evaluations of expr.
+   Collections are iterated in a nested fashion, rightmost fastest,
+   and nested coll-exprs can refer to bindings created in prior
+   binding-forms.  Supported modifiers are: :let [binding-form expr ...],
+   :while test, :when test.
+
+  Unlike clojure.core/for, expansion grows linearly with the number
+  of nestings of this macro, rather than exponentially.
+
+  (take 100 (for [x (range 100000000) y (range 1000000) :while (< y x)] [x y]))"
+  {:added "1.0"}
+  [seq-exprs body-expr]
+  (assert-args
+    (vector? seq-exprs) "a vector for its binding"
+    (even? (count seq-exprs)) "an even number of forms in binding vector")
+  (let [to-groups (fn [seq-exprs]
+                    (reduce (fn [groups [k v]]
+                              (if (keyword? k)
+                                (conj (pop groups) (conj (peek groups) [k v]))
+                                (conj groups [k v])))
+                            [] (partition 2 seq-exprs)))
+        err (fn [& msg] (throw (IllegalArgumentException. ^String (apply str msg))))
+        emit-bind (fn emit-bind [[[bind expr & mod-pairs]
+                                  & [[_ next-expr] :as next-groups]]]
+                    (let [giter (gensym "iter__")
+                          gxs (gensym "s__")
+                          gfirst (gensym "first__")
+                          gf (gensym "f__")
+                          gi (gensym "i__")
+                          gb (gensym "b__")
+                          inner-most-loop? (nil? next-groups)
+                          do-mod (fn do-mod [chunked? [[k v :as pair] & etc]]
+                                   {:pre [(or (not chunked?) inner-most-loop?)]}
+                                   (let [wrap (fn [b] (if inner-most-loop? `(let [~bind (~gf ~gfirst)] ~b) b))]
+                                     (cond
+                                       (= k :let) (wrap `(let ~v ~(do-mod chunked? etc)))
+                                       (= k :while) (wrap `(when ~v ~(do-mod chunked? etc)))
+                                       (= k :when) (wrap `(if ~v
+                                                            ~(do-mod chunked? etc)
+                                                            (recur ~(if chunked?
+                                                                      `(unchecked-inc ~gi)
+                                                                      `(rest ~gxs)))))
+                                       (keyword? k) (err "Invalid 'for' keyword " k)
+                                       (not inner-most-loop?) `(let [iterys# ~(emit-bind next-groups)]
+                                                                 (if-let [fs# (seq (iterys# ~next-expr))]
+                                                                   (concat fs# (~giter (rest ~gxs)))
+                                                                   (recur (rest ~gxs))))
+                                       :else (if chunked?
+                                               `(do (chunk-append ~gb (~gf ~gfirst))
+                                                    (recur (unchecked-inc ~gi)))
+                                               `(cons (~gf ~gfirst)
+                                                      (~giter (rest ~gxs)))))))]
+                      `(fn ~giter [~gxs]
+                         (lazy-seq
+                           (loop [~gxs ~gxs]
+                             ~(if-not inner-most-loop?
+                                `(when-first [~bind ~gxs]
+                                   ~(do-mod false mod-pairs))
+                                `(when-let [~gxs (seq ~gxs)]
+                                   (let [~gf (fn [~bind] ~body-expr)]
+                                     (if (chunked-seq? ~gxs)
+                                       (let [c# (chunk-first ~gxs)
+                                             size# (int (count c#))
+                                             ~gb (chunk-buffer size#)]
+                                         (if (loop [~gi (int 0)]
+                                               (if (< ~gi size#)
+                                                 (let [~gfirst (.nth c# ~gi)]
+                                                   ~(do-mod true mod-pairs))
+                                                 true))
+                                           (chunk-cons
+                                             (chunk ~gb)
+                                             (~giter (chunk-rest ~gxs)))
+                                           (chunk-cons (chunk ~gb) nil)))
+                                       (let [~gfirst (first ~gxs)]
+                                         ~(do-mod false mod-pairs)))))))))))]
+    `(let [iter# ~(emit-bind (to-groups seq-exprs))]
+       (iter# ~(second seq-exprs)))))
---
+++ test/io/github/frenchy64/fully_satisfies/linear_expansion_test.clj
@@ -0,0 +1,178 @@
+(ns io.github.frenchy64.fully-satisfies.linear-expansion-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [clojure.core :as c]
+            [io.github.frenchy64.fully-satisfies.linear-expansion :as fixed]))
+
+(def ^:dynamic *counter* nil)
+(defmacro counting-macro [] (swap! *counter* inc))
+
+(defn is-count-expansions [expected form]
+  (binding [*counter* (atom 0)]
+    (eval form)
+    (is (= expected @*counter*))))
+
+(deftest counting-macro-test
+  (is-count-expansions 0 `(do))
+  (is-count-expansions 1 `(counting-macro)))
+
+(deftest doseq-expands-exponentially-test
+  (is-count-expansions 1 `(counting-macro))
+  (is-count-expansions 1 `(c/doseq [] (counting-macro)))
+  (is-count-expansions 2 `(c/doseq [_# nil] (counting-macro)))
+  (is-count-expansions 16 `(c/doseq [_# nil _# nil _# nil _# nil] (counting-macro)))
+  (is-count-expansions 8 `(c/doseq [_# nil _# nil _# nil _# (counting-macro)]))
+  (is-count-expansions 15 `#(c/doseq [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)]))
+  (is-count-expansions 31 `#(c/doseq [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)] (counting-macro)))
+  (is-count-expansions 7 `#(c/doseq [_# (counting-macro) _# (counting-macro)] (counting-macro)))
+  (is-count-expansions 5 `#(c/doseq [_# (counting-macro) :when (counting-macro)] (counting-macro)))
+  (is-count-expansions 5 `#(c/doseq [_# (counting-macro) :while (counting-macro)] (counting-macro)))
+  (is-count-expansions 5 `#(c/doseq [_# (counting-macro) :let [_# (counting-macro)]] (counting-macro))))
+
+(deftest doseq-single-variable-test
+  (is (macroexpand-1 `(doseq [~'v ~'e] ~'body))))
+
+(deftest fixed-doseq-duplicates-no-expansion-test
+  (is-count-expansions 1 `(counting-macro))
+  (is-count-expansions 1 `(fixed/doseq [] (counting-macro)))
+  (is-count-expansions 1 `(fixed/doseq [_# nil] (counting-macro)))
+  (is-count-expansions 1 `(fixed/doseq [_# nil _# nil _# nil _# nil] (counting-macro)))
+  (is-count-expansions 1 `(fixed/doseq [_# nil _# nil _# nil _# (counting-macro)]))
+  (is-count-expansions 4 `#(fixed/doseq [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)]))
+  (is-count-expansions 5 `#(fixed/doseq [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)] (counting-macro)))
+  (is-count-expansions 3 `#(fixed/doseq [_# (counting-macro) _# (counting-macro)] (counting-macro)))
+  (is-count-expansions 3 `#(fixed/doseq [_# (counting-macro) :when (counting-macro)] (counting-macro)))
+  (is-count-expansions 3 `#(fixed/doseq [_# (counting-macro) :while (counting-macro)] (counting-macro)))
+  (is-count-expansions 3 `#(fixed/doseq [_# (counting-macro) :let [_# (counting-macro)]] (counting-macro)))
+)
+
+(defn c-doseq-1 [c f] (c/doseq [i c] (f ::c/doseq i)))
+(defn f-doseq-1 [c f] (fixed/doseq [i c] (f ::fixed/doseq i)))
+(defn c-doseq-2 [c1 c2 f] (c/doseq [i c1 j c2] (f ::c/doseq [i j])))
+(defn f-doseq-2 [c1 c2 f] (fixed/doseq [i c1 j c2] (f ::fixed/doseq [i j])))
+(defn c-doseq-3 [c1 c2 c3 f] (c/doseq [i c1 j c2 k c3] (f ::c/doseq [i j k])))
+(defn f-doseq-3 [c1 c2 c3 f] (fixed/doseq [i c1 j c2 k c3] (f ::fixed/doseq [i j k])))
+
+(deftest fixed-doseq-parity-test
+  (doseq [c1 [[0 1]
+              (range 2)
+              (sorted-set 0 1)]
+          :let [_ (testing "1 variable"
+                    (let [a (atom {})
+                          f #(swap! a update %1 (fnil conj []) %2)
+                          expected [0 1]]
+                      (c-doseq-1 c1 f)
+                      (f-doseq-1 c1 f)
+                      (is (= {::c/doseq expected ::fixed/doseq expected} @a))))]
+          c2 [[1 0]
+              (range 1 -1 -1)
+              (sorted-set-by (comp - compare) 0 1)]]
+    (testing "2 variables"
+      (let [a (atom {})
+            f #(swap! a update %1 (fnil conj []) %2)
+            expected [[0 1] [0 0] [1 1] [1 0]]]
+        (c-doseq-2 c1 c2 f)
+        (f-doseq-2 c1 c2 f)
+        (is (= {::c/doseq expected ::fixed/doseq expected} @a))))
+    (testing "3 variables"
+      (let [a (atom {})
+            c3 c2
+            f #(swap! a update %1 (fnil conj []) %2)
+            expected [[0 1 1] [0 1 0] [0 0 1] [0 0 0]
+                      [1 1 1] [1 1 0] [1 0 1] [1 0 0]]]
+        (c-doseq-3 c1 c2 c3 f)
+        (f-doseq-3 c1 c2 c3 f)
+        (is (= {::c/doseq expected ::fixed/doseq expected} @a)))))
+  (let [total-iterations (atom {::c/doseq 0 ::fixed/doseq 0})
+        expected-iterations 182250
+        size 10]
+    (doseq [i (range size)
+            j (range size)
+            k (range size)
+            b1 [true false]
+            b2 [true false]
+            :let [c1 (range i)
+                  c2 (range j)
+                  c3 (range k)]]
+      (assert (not (Thread/interrupted)))
+      (let [a (atom {::c/doseq [] ::fixed/doseq []})
+            f #(swap! a update %1 conj %2)
+            _ (c/doseq [i c1 :when b1 j c2 :let [foo (if b2 c1 c2)] k c3]
+                (swap! total-iterations update ::c/doseq inc)
+                (f ::c/doseq [i j k foo]))
+            _ (fixed/doseq [i c1 :when b1 j c2 :let [foo (if b2 c1 c2)] k c3]
+                (swap! total-iterations update ::fixed/doseq inc)
+                (f ::fixed/doseq [i j k foo]))
+            {cdoseq ::c/doseq
+             fdoseq ::fixed/doseq} @a]
+        (assert (and cdoseq fdoseq))
+        (is (= cdoseq fdoseq))))
+    (is (= {::c/doseq expected-iterations
+            ::fixed/doseq expected-iterations} @total-iterations))))
+
+(deftest fixed-doseq-single-variable-test
+  (is (macroexpand-1 `(fixed/doseq [~'v ~'e] ~'body)))
+  )
+
+(deftest for-expands-exponentially-test
+  (is (thrown? Exception (eval `(c/for [] (counting-macro)))))
+  (is-count-expansions 2 `(c/for [_# nil] (counting-macro)))
+  (is-count-expansions 2 `(c/for [_# nil _# nil _# nil _# nil] (counting-macro)))
+  (is-count-expansions 1 `(c/for [_# nil _# nil _# nil _# (counting-macro)] _#))
+  (is-count-expansions 4 `(c/for [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)] _#))
+  (is-count-expansions 16 `(c/for [_# nil] (c/for [_# nil] (c/for [_# nil] (c/for [_# nil] (counting-macro))))))
+  (is-count-expansions 64 `(c/for [_# nil] (c/for [_# nil] (c/for [_# nil] (c/for [_# nil] (c/for [_# nil] (c/for [_# nil] (counting-macro))))))))
+  (is-count-expansions 6 `(c/for [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)] (counting-macro))))
+
+(deftest for-single-variable-test
+  (is (macroexpand-1 `(c/for [~'v ~'e] ~'body))))
+
+(deftest fixed-for-duplicates-no-expansion-test
+  (is (thrown? Exception (eval `(fixed/for [] (counting-macro)))))
+  (is-count-expansions 1 `(fixed/for [_# nil] (counting-macro)))
+  (is-count-expansions 1 `(fixed/for [_# nil _# nil _# nil _# nil] (counting-macro)))
+  (is-count-expansions 1 `(fixed/for [_# nil _# nil _# nil _# (counting-macro)] _#))
+  (is-count-expansions 4 `(fixed/for [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)] _#))
+  (is-count-expansions 1 `(fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (counting-macro))))))
+  (is-count-expansions 1 `(fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (counting-macro)))))))
+  (is-count-expansions 1 `(fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (fixed/for [_# nil] (counting-macro))))))))
+  (is-count-expansions 5 `(fixed/for [_# (counting-macro) _# (counting-macro) _# (counting-macro) _# (counting-macro)] (counting-macro))))
+
+(deftest fixed-for-single-variable-test
+  (is (macroexpand-1 `(fixed/for [~'v ~'e] ~'body))))
+
+(deftest fixed-for-parity-test
+  (doseq [c1 [[0 1]
+              (range 2)
+              (sorted-set 0 1)]
+          :let [_ (testing "1 variable"
+                    (is (= [0 1]
+                           (c/for [i c1] i)
+                           (fixed/for [i c1] i))))]
+          c2 [[1 0]
+              (range 1 -1 -1)
+              (sorted-set-by (comp - compare) 0 1)]]
+    (testing "2 variables"
+      (is (= [[0 1] [0 0] [1 1] [1 0]]
+             (c/for [i c1 j c2] [i j])
+             (fixed/for [i c1 j c2] [i j]))))
+    (testing "3 variables"
+      (let [c3 c2]
+        (is (= [[0 1 1] [0 1 0] [0 0 1] [0 0 0]
+                [1 1 1] [1 1 0] [1 0 1] [1 0 0]]
+               (c/for [i c1 j c2 k c3] [i j k])
+               (fixed/for [i c1 j c2 k c3] [i j k]))))))
+  (let [size 10]
+    (doseq [i (range size)
+            j (range size)
+            k (range size)
+            b1 [true false]
+            b2 [true false]
+            :let [c1 (range i)
+                  c2 (range j)
+                  c3 (range k)]]
+      (assert (not (Thread/interrupted)))
+      (let [core (c/for [i c1 :when b1 j c2 :let [foo (if b2 c1 c2)] k c3]
+                   [i j k foo])
+            fixed (fixed/for [i c1 :when b1 j c2 :let [foo (if b2 c1 c2)] k c3]
+                    [i j k foo])]
+        (is (= core fixed))))))
